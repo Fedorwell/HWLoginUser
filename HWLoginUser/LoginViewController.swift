1eec63453ac98d8a92db84ebf429b2e16ca737d4
Fedorwell <115953771+Fedorwell@users.noreply.github.com>
2023-05-10 23:34:55 +0300

    Add func unwindSegueToMainScreen

--- HWLoginUser/LoginViewController.swift
+++ HWLoginUser/LoginViewController.swift
@@ -4,36 +4,8 @@
 //
 //  Created by mac on 09.05.2023.
 //
-//Создайте приложение с двумя экранами:
 
-//1. На первом экране LoginViewController реализуйте авторизацию пользователя с использованием текстовых полей для ввода имени пользователя и пароля.
-//
-//2. Настройте соответствующие параметры для клавиатуры: отключите автокоррекцию и предиктивный ввод, вводимые символы в поле для пароля должны скрываться. Обеспечьте условия, при которых клавиатура не перекрывает элементы интерфейса на экране.
-//
-//3. Реализуйте скрытие клавиатуры при тапе по экрану, переопределив метод touchesBegan:
-//
-// Метод для скрытия клавиатуры тапом по экрану
-//override func touchesBegan(_ touches: Set, with event: UIEvent?) {
-//    super .touchesBegan(touches, with: event)
-//}
-//4. Валидируйте данные пользовательского ввода, используя метод shouldPerformSegue(withIdentifier:sender:). Этот метод вызывается перед выполнением перехода, и если он возвращает false, переход не состоится:
-//
-//override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-//    guard userNameTextField.text == user, passwordTextField.text == password else {
-//        // Введенное имя не валидно, отменяем переход и показываем алерт контроллер
-//        return false
-//    }
-//
-//    // Введенное имя валидно, разрешаем переход
-//    return true
-//}
-//5. В случае успешной авторизации перейдите на второй экран WelcomeViewController, где отобразите приветствие с именем пользователя и кнопку "логаута" для возврата на первый экран.
-//
-//6. Передайте имя пользователя между экранами, используя метод prepare.
-//
-//7. При возврате на первый экран очистите текстовые поля для ввода имени пользователя и пароля, используя метод unwind.
-//
-//8. Для вызова или скрытия клавиатуры в симуляторе используйте сочетание клавиш Command + K.
+
 import UIKit
 
 final class LoginViewController: UIViewController {
@@ -66,27 +38,29 @@ final class LoginViewController: UIViewController {
     }
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        guard userNameTextField.text == "user"  else {
-            // Введенное имя не валидно, отменяем переход и показываем алерт контроллер
+        let username = "user"
+        let password = "password"
+        guard userNameTextField.text == username  else {
+        
             showAlert(withTitle: "Username is not correct!", andMessage: "Try again!")
             return false
         }
         
-        guard passwordTextField.text == "password" else {
+        guard passwordTextField.text == password else {
             showAlert(withTitle: "Password is not correct!", andMessage: "Try again!")
             return false
         }
-        // Введенное имя валидно, разрешаем переход
+       
         return true
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-       guard let settingsVC = segue.destination as? WelcomeViewController else { return }
+        guard let settingsVC = segue.destination as? WelcomeViewController else { return }
         settingsVC.hellowUserValue = userNameTextField.text
     }
     
     @IBAction func forgotUNButton() {
-       showAlert(withTitle: "User name is:", andMessage: "user")
+        showAlert(withTitle: "User name is:", andMessage: "user")
     }
     
     @IBAction func forgotPassButton() {
@@ -96,11 +70,13 @@ final class LoginViewController: UIViewController {
     @IBAction func loginTappedButton() {
         
     }
-    
-    
+    @IBAction func unwindSegueToMainScreen(segue: UIStoryboardSegue) {
+        guard let svc = segue.source as? WelcomeViewController else { return }
+        self.userNameTextField.text = svc.clearField
+        self.passwordTextField.text = svc.clearField
+    }
     
 }
-
 extension LoginViewController {
     private func showAlert(withTitle title: String, andMessage message: String) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
@@ -111,3 +87,6 @@ extension LoginViewController {
         present(alert, animated: true)
     }
 }
+
+
+
--- HWLoginUser/WelcomeViewController.swift
+++ HWLoginUser/WelcomeViewController.swift
@@ -8,19 +8,29 @@
 import UIKit
 
 final class WelcomeViewController: UIViewController {
-
+    
     @IBOutlet var hellowUserField: UILabel!
+    @IBOutlet var logoutButton: UIButton!
     
     var hellowUserValue: String!
+    let clearField = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         hellowUserField.text = "Hello, \(hellowUserValue!)!"
+        
+        logoutButton.layer.cornerRadius = 10
     }
     
-
+    
     @IBAction func logoutButtonTapped() {
         dismiss(animated: true)
+        
     }
     
 }
+
+
+
+
+
